8a916af6f1fe62f7db7ffa55e0da74380b5dd370
Julien Ergan <julien.ergan.code@gmail.com>
2020-09-25 22:51:26 +0900

    initial commit with boilerplate - gem webpacker - rails react connection - API until controller action create post

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root to: 'pages#home'
+
+  get "/posts/new", to: 'pages#home'
+  get "/posts/:id", to: 'pages#home'
+  
+  # API
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :posts, only: [ :index, :show, :create ]
+    end
+  end
+end
